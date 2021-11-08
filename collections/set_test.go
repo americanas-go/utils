8b8d59d8a716151297dec3e59f5fa7a72f78cc0f
Rodolfo Moi de Oliveira <rodmoi.oliveira@gmail.com>
2021-11-08 16:53:57 -0300

    feat: add new functionality for sets
    * feat: add Clear and IsEmpty methods to Set type
    
    * feat: add Collect and Intersection methods to Set type
    
    * feat: add Union method to Set type
    
    * feat: add Difference method to Set type
    
    * feat: add SymmetricDifference method to Set type

--- collections/set_test.go
+++ collections/set_test.go
@@ -35,3 +35,81 @@ func TestRemove(t *testing.T) {
 	ok = set.Remove("cat")
 	assert.Equal(t, false, ok, "they should be equal")
 }
+
+func TestIntersection(t *testing.T) {
+	set1 := MakeSet("cat", "dog", "cow")
+	set2 := MakeSet("cat", "duck", "bull")
+	intersection := set1.Intersection(set2)
+
+	assert.Contains(t, intersection, "cat")
+	assert.NotContains(t, intersection, "dog")
+	assert.NotContains(t, intersection, "cow")
+	assert.NotContains(t, intersection, "duck")
+	assert.NotContains(t, intersection, "bull")
+}
+
+func TestSymmetricDifference(t *testing.T) {
+	set1 := MakeSet(1, 2, 3)
+	set2 := MakeSet(4, 2, 3, 4)
+
+	symDiff := set1.SymmetricDifference(set2)
+	assert.Contains(t, symDiff, 1)
+	assert.Contains(t, symDiff, 4)
+	assert.NotContains(t, symDiff, 2)
+	assert.NotContains(t, symDiff, 3)
+}
+
+func TestDifference(t *testing.T) {
+	set1 := MakeSet(1, 2, 3)
+	set2 := MakeSet(4, 2, 3, 4)
+
+	diff1 := set1.Difference(set2)
+	assert.Contains(t, diff1, 1)
+	assert.NotContains(t, diff1, 2)
+	assert.NotContains(t, diff1, 3)
+	assert.NotContains(t, diff1, 4)
+
+	diff2 := set2.Difference(set1)
+	assert.Contains(t, diff2, 4)
+	assert.NotContains(t, diff2, 1)
+	assert.NotContains(t, diff2, 2)
+	assert.NotContains(t, diff2, 3)
+}
+
+func TestUnion(t *testing.T) {
+	set1 := MakeSet("cat", "dog", "cow")
+	set2 := MakeSet("cat", "duck", "bull")
+	intersection := set1.Union(set2)
+
+	assert.Contains(t, intersection, "cat")
+	assert.Contains(t, intersection, "dog")
+	assert.Contains(t, intersection, "cow")
+	assert.Contains(t, intersection, "duck")
+	assert.Contains(t, intersection, "bull")
+}
+
+func TestCollect(t *testing.T) {
+	els := []interface{}{"cat", "cow", 10, true, false, 10, true, false, "cat", "cow"}
+	setValues := MakeSet(els...).Collect()
+
+	assert.Contains(t, setValues, "cat")
+	assert.Contains(t, setValues, 10)
+	assert.Contains(t, setValues, "cow")
+	assert.Contains(t, setValues, true)
+	assert.Contains(t, setValues, false)
+}
+
+func TestClear(t *testing.T) {
+	set := MakeSet("cat", "dog", "cow")
+
+	set.Clear()
+	assert.Equal(t, 0, len(set), "they should be equal")
+}
+
+func TestIsEmpty(t *testing.T) {
+	set := MakeSet()
+	assert.Equal(t, true, set.IsEmpty(), "they should be equal")
+
+	set.Insert("cat")
+	assert.Equal(t, false, set.IsEmpty(), "they should be equal")
+}
